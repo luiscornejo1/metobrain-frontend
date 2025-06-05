71d5fa1a39f30a047a625905e6e579db30dff427
LukeCharles <luiscarlocornejor@live.com>
2025-06-05 03:18:50 -0500

    fix :subiendo cambios al repo PARA NETLIFY

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api', // Reemplaza con tu URL real
+  baseURL: import.meta.env.VITE_API_URL, // Reemplaza con tu URL real
   headers: {
     'Content-Type': 'application/json'
   }
